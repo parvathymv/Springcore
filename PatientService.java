78b79b4afb2cab359a90dfbbe0a75d2fa1fef805
parvathymv <63205325+parvathymv@users.noreply.github.com>
2024-11-22 15:47:02 +0530

    Add files via upload

---
+++ PatientService.java
@@ -0,0 +1,28 @@
+package com.service;
+
+import testproj.beans.Patient;
+
+public class PatientService {
+
+	// @Autowired
+
+	private Patient patient;
+
+	public Patient getPatient() {
+		return patient;
+	}
+
+	public void setPatient(Patient patient) {
+		this.patient = patient;
+	}
+
+	public void getPatientDetails() {
+		System.out.println("Patient Id : " + patient.getPid());
+		System.out.println("Patient Name : " + patient.getPname());
+		System.out.println("Disease : " + patient.getDisease());
+		System.out.println("Gender : " + patient.getGender());
+		System.out.println("Admit Status : " + patient.getAdmit_status());
+		System.out.println("Age : " + patient.getAge());
+	}
+
+}
